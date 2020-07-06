f75f1e1a619ac56977a199c09b3c8d9d111dd57e
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-07-06 15:02:51 -0700

    Updated Article.js with revised code for Task 1.

--- components/Article.js
+++ components/Article.js
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Lorem Ipsum',
+    date: 'July 6th, 2020',
+    firstParagraph: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. `,
+
+    secondParagraph: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+
+    thirdParagraph: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
   }
 ];
 
@@ -114,7 +123,6 @@ const data = [
 
 const articlesDiv = document.querySelector(`.articles`)
 
-
 const articleMaker = (obj => {
   let article = document.createElement(`div`)
   article.classList.add(`article`)
@@ -139,20 +147,12 @@ const articleMaker = (obj => {
   articleExpand.classList.add(`expandButton`)
   articleExpand.textContent = `+`
   article.appendChild(articleExpand)
+  // console.log(article)
+  article.addEventListener(`click`, (a) => {
+    a.target.parentNode.classList.toggle(`article-open`)
+  })
 
   return article
-})
-// const expandButton = document.querySelectorAll(`.expandButton`)
-
-const expandButton = document.getElementsByClassName(`expandButton`)
-// const expandArray = Array.from(expandButton)
-console.log(expandButton)
-// console.log(expandArray)
-
-expandButton.forEach((item) => {
-  item.addEventListener(`click`, () => {
-  expandButton.classList.toggle(`article-open`)
-})
 })
 
 data.forEach(articleMaker)
